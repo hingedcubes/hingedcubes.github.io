2c4b52e940b98615e5df7ca377bef2c87f96ebbc
Kap-ryeong kim <kap@hingedcubes.com>
2020-07-20 17:33:33 +0900

    dotnet build tool

---
+++ build_dotnet.ps1
@@ -0,0 +1,7 @@
+$env:BASYX_REPO = "$(Get-Location)..\sdks\dotnet\basyx-packages"
+Get-ChildItem $env:USERPROFILE\.nuget -Directory -Filter basyx* -Recurse | Remove-Item -force -Recurse
+dotnet build ..\sdks\dotnet\basyx-core\BaSyx.Core.sln
+dotnet build ..\sdks\dotnet\basyx-components\BaSyx.AAS.Client.Http\BaSyx.AAS.Client.Http.csproj
+dotnet build ..\sdks\dotnet\basyx-components\BaSyx.Components.sln
+
+
